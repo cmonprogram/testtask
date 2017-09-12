1fff87a556dd451a74aaab23c452d57ab4be453a
VLAD <vlad5956@gmail.com>
2017-09-12 22:05:30 +0300

    init

---
+++ basic/views/site/entry.php
@@ -0,0 +1,17 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<div class="col-xs-4">
+<?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+<?php ActiveForm::end(); ?>
+</div>
